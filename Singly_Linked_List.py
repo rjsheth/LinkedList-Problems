f71aa757c53be2109660da6361e0fca5ef5b7c94
Ruchi Sheth <rjsheth@ucsc.edu>
2018-01-02 09:58:34 -0800

    Create Singly_Linked_List.py
    Singly linked list with add and delete

---
+++ Singly_Linked_List.py
@@ -0,0 +1,77 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class Singly_Linked_List:
+    def __init__(self):
+        self.head = None
+
+    def add(self,data): # push_back # pushing at the end of Singly_Linked_List
+        node_elem = Node(data)
+        if(self.head == None):
+            self.head = node_elem
+        else:
+            currNode = self.head
+            while(currNode.next != None):
+                currNode = currNode.next
+                # print(currNode.data)
+            currNode.next = node_elem
+            # print(currNode.data)
+            node_elem.next = None
+            # print(currNode.next.data)
+            # print(node_elem.next)
+
+    def delete(self,data):
+        currNode = self.head
+        prevNode = self.head
+        nextNode = None
+        if (currNode != None):
+            nextNode = currNode.next
+        while(currNode != None):
+            if(currNode.data == data):
+                print(currNode.data);
+                # if(prevNode ==self.head)
+                # break
+                if(prevNode == self.head):
+                    self.head = nextNode
+                    prevNode = self.head
+                    #print(prevNode.data,"pooo")
+                else:
+                    #print(prevNode.data,"kooo")
+                    prevNode.next = nextNode
+            else:
+                prevNode = currNode
+            currNode = nextNode
+            if(currNode != None):
+                nextNode = currNode.next
+            # if(currNode.data == 1):
+            #     print(prevNode.data)
+            #     print(currNode.data)
+            #     print(nextNode.next)
+            #     break
+
+    def __str__(self):
+        currNode = self.head
+        s = ''
+        # if(currNode.next != None):
+        while(currNode != None):
+            # print(currNode.data)
+            s += str(currNode.data)
+            # if(currNode.next != None):
+            s+= '=>'
+            currNode = currNode.next
+        s += str(currNode)
+        return s
+    def print(list):
+        currNode = list.head
+        s = ''
+        # if(currNode.next != None):
+        while(currNode != None):
+            # print(currNode.data)
+            s += str(currNode.data)
+            # if(currNode.next != None):
+            s+= '=>'
+            currNode = currNode.next
+        s += str(currNode)
+        return s
